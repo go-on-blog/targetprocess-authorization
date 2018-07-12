449b8dba443c14d4aaf3d365583793a9d30c30d7
Guillaume Oriol <guillaume.oriol@carestream.com>
2018-07-12 15:55:14 +0200

    Add getAll function and make it available to users.js

---
+++ test/lib/domain/getAll_test.js
@@ -0,0 +1,89 @@
+/*jslint
+    es6
+*/
+"use strict";
+
+const {describe, it} = require("mocha");
+
+describe("getAll", function () {
+    const chai = require("chai");
+    const expect = chai.expect;
+    const chaiAsPromised = require("chai-as-promised");
+    const sinon = require("sinon");
+    const factory = require("../../../lib/domain/getAll");
+    const credentials = require("../../credentials");
+    const config = Object.assign({resource: "Users"}, credentials);
+
+    chai.use(chaiAsPromised);
+
+    describe("getAll", function () {
+        function getSUT(args, value) {
+            const request = sinon.stub();
+            const retriever = require("targetprocess-api/retrieve")(Object.assign({request}, config));
+            const stampit = require("@stamp/it");
+            const stamp = stampit(factory, {props: {retriever}});
+
+            request.rejects();
+            request.withArgs(args).resolves(value);
+
+            return stamp();
+        }
+
+        it("should return an empty array when no item is found", function () {
+            const args = {
+                method: "GET",
+                uri: `https://${credentials.domain}/api/v1/Users/`,
+                qs: {
+                    token: credentials.token,
+                    include: "[Id]",
+                    take: 1000
+                },
+                json: true
+            };
+            const sut = getSUT(args, {Items: []});
+
+            return expect(sut.getAll())
+                .to.eventually.be.an("array")
+                .and.to.be.empty;
+        });
+
+        it("should return an array of objects when items are found", function () {
+            const args = {
+                method: "GET",
+                uri: `https://${credentials.domain}/api/v1/Users/`,
+                qs: {
+                    token: credentials.token,
+                    include: "[Id]",
+                    take: 1000
+                },
+                json: true
+            };
+            const expected = [{Id: 42}];
+            const sut = getSUT(args, {Items: expected});
+
+            return expect(sut.getAll())
+                .to.eventually.be.an("array")
+                .and.to.have.lengthOf(1)
+                .and.to.have.deep.members(expected);
+        });
+
+        it("should return an array of objects including the specified properties", function () {
+            const args = {
+                method: "GET",
+                uri: `https://${credentials.domain}/api/v1/Users/`,
+                qs: {
+                    token: credentials.token,
+                    include: "[Id,Login]",
+                    take: 1000
+                },
+                json: true
+            };
+            const expected = [{Id: 42, Login: "Deep Thought"}];
+            const sut = getSUT(args, {Items: expected});
+
+            return expect(sut.getAll(["Id", "Login"]))
+                .to.eventually.be.an("array")
+                .and.to.have.deep.members(expected);
+        });
+    });
+});
--- test/lib/domain/users_test.js
+++ test/lib/domain/users_test.js
@@ -15,19 +15,19 @@ describe("users", function () {
 
     chai.use(chaiAsPromised);
 
+    function getSUT(args, value) {
+        const request = sinon.stub();
+        const retriever = require("targetprocess-api/retrieve")(Object.assign({request, resource: "Users"}, credentials));
+        const stampit = require("@stamp/it");
+        const stamp = stampit(factory, {props: {retriever}});
+
+        request.rejects();
+        request.withArgs(args).resolves(value);
+
+        return stamp();
+    }
+
     describe("getId", function () {
-        function getSUT(args, value) {
-            const request = sinon.stub();
-            const retriever = require("targetprocess-api/retrieve")(Object.assign({request, resource: "Users"}, credentials));
-            const stampit = require("@stamp/it");
-            const stamp = stampit(factory, {props: {retriever}});
-
-            request.rejects();
-            request.withArgs(args).resolves(value);
-
-            return stamp();
-        }
-
         it("should return the user id matching the specified last name", function () {
             const lastName = "x";
             const args = {
@@ -44,6 +44,27 @@ describe("users", function () {
             return expect(sut.getId(lastName))
                 .to.eventually.be.a("number")
                 .and.to.equal(42);
+        });
+    });
+
+    describe("getAll", function () {
+        it("should return the user id matching the specified last name", function () {
+            const args = {
+                method: "GET",
+                uri: `https://${credentials.domain}/api/v1/Users/`,
+                qs: {
+                    token: credentials.token,
+                    include: "[Id]",
+                    take: 1000
+                },
+                json: true
+            };
+            const expected = [{Id: 42}];
+            const sut = getSUT(args, {Items: expected});
+
+            return expect(sut.getAll(["Id"]))
+                .to.eventually.be.an("array")
+                .and.to.have.deep.members(expected);
         });
     });
 });
